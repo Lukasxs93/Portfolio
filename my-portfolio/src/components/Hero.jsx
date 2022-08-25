52911c98ba317064d843388997b91231b6fd313b
Luca Porcelli <87008874+Lukasxs93@users.noreply.github.com>
2022-08-25 14:43:41 +0100

    typed

--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -1,19 +1,20 @@
-import Typewriter from 'typewriter-effect';
+
+import Typed from 'react-typed';
 function Hero() {
   
     return ( 
         <div className="hero">
-             <Typewriter
-             options={{
-               strings:['Hi my name is Luca and i like: Coding wonderfoul web apps',
+             <Typed
+             
+               strings={['Hi my name is Luca and i like: Coding wonderfoul web apps',
 		'Hi my name is Luca and i like: 🎣Fishing🎣',
 		'Hi my name is Luca and i like: 🦜🐱 pets 🐶🐠',
 		'Hi my name is Luca and i like: Perplexing my Arduino ©️',
 		'Hi my name is Luca and i like: The Matrix 🔵💊🔴',
-		'Hi my name is Luca and i like: New Challenges!',],
-          autoStart:true,
-           deleteSpeed:50     
-             }}            
+		'Hi my name is Luca and i like: New Challenges!',]}
+          typeSpeed={30}
+           backSpeed={50}    
+                         
              />
             
         
