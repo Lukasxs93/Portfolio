92a3f5d9abb889d1b7c43281e021b161c0686a56
Lukasxs93 <lucaporcelli93@gmail.com>
2022-09-07 12:38:35 +0100

    not mutch hiking

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
                And yet, I did start University studying computing and IT practices, I am fascinated by the complexity of data structures, and 
                amazed from the simplicity of high level programming languages. 
                Hi, my name is Luca, and for a brief moment last summer i was quite famous, thanks to the Pixar Studios. 
-               I am an Italian guy, living in the beautyfull valleys of Wales, where i practice my fly fishing skills, and hiking (not so mush skills )
+               I am an Italian guy, living in the beautyfull valleys of Wales, where i practice my fly fishing skills, and hiking 
             </p>
         </main>
      );
